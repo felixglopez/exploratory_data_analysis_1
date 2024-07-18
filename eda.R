42958d9bdf3a9cd25a140dd7cd384c21a559598a
Felix G Lopez <felixglopez@gmail.com>
2024-07-18 13:03:16 -0300

    creating plot 1

--- eda.R
+++ eda.R
@@ -3,36 +3,24 @@
 setwd("/Users/fglopez/Documents/GitHub/exploratory_data_analysis_1")
 
 #unizipping my file and loading
-hpc <- read.table("~/Documents/GitHub/exploratory_data_analysis/household_power_consumption.txt", 
-                  header = TRUE, sep = ";", na.strings = "?")
+library(utils)
+
+unzip("/Users/fglopez/Documents/GitHub/exploratory_data_analysis_1/exdata_data_household_power_consumption.zip")
+
+#reading the file
+file_name <- "household_power_consumption.txt"
+hpc <- read.table(file_name, header = TRUE, sep = ";", dec = ".", na.strings = "?")
 
 #analysing the file
 head(hpc)
-names(hpc)
 str(hpc)
 dim(hpc)
 
 #subsetting data from dates
 hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"), ]
-
-
 
 
 #PLOT 1
-hpc <- read.table("~/Documents/GitHub/exploratory_data_analysis/household_power_consumption.txt", 
-                  header = TRUE, sep = ";", na.strings = "?")
-
-#analysing the file
-head(hpc)
-names(hpc)
-str(hpc)
-dim(hpc)
-
-#subsetting data from dates
-hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"), ]
-
-
-
 par(mfrow = c(1,1))
 hist(hpc[, 3], col = "red", main = "Global Active Power", 
      xlab = "Global Active Power (kilowatts)")
